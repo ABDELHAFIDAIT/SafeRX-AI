de6c7d7b7a69b1fabf1418ed81e9070ae6bf485f
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-03-13 12:32:20 +0000

    feat: register new routers in main api router

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from backend.app.api.endpoints import auth, account, patients, prescriptions, drugs
+from backend.app.api.endpoints import auth, account, patients, prescriptions, drugs, audit
 
 
 router = APIRouter()
@@ -9,3 +9,4 @@ router.include_router(account.router, prefix="/account", tags=["account"])
 router.include_router(patients.router, prefix="/patients", tags=["patients"])
 router.include_router(prescriptions.router, prefix="/prescriptions", tags=["prescriptions"])
 router.include_router(drugs.router, prefix="/drugs", tags=["drugs"])
+router.include_router(audit.router, prefix="/audit", tags=["Audit"])
